e500dcf9adc0a40fc38aae0105889c922ac4e5c3
sunfoxy2k <sunfoxy2k@gmail.com>
2026-08-01 12:27:02 +0700

    setup test ci/cd

---
+++ src/test/auth.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+    isActive: true,
+    age: 32,
+}
+
+describe("person", () => {
+    test("should be defined", () => {
+        expect(person).toBeDefined();
+    });
+
+    test("should be active", () => {
+        expect(person.isActive).toBe(true);
+    });
+
+    
+});

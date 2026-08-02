55f19016f57ae7ffa8233e8d776db33e9a744388
sunfoxy2k <sunfoxy2k@gmail.com>
2026-08-02 15:55:07 +0700

    add eslint-plugin-security for enhanced security linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -11,4 +12,5 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);

c7672ae77d60355c275008e91ee709e8aaf7c425
sunfoxy2k <42714659+sunfoxy2k@users.noreply.github.com>
2026-08-02 18:54:19 +0700

    setup ci-cd with pnpm
    * add style
    
    * setup fail lint
    
    * fix lint
    
    * update to use pnpm
    
    * add eslint-plugin-security for enhanced security linting
    
    * ci: fail build on warning
    
    * fix ci build issue
    
    * hotfix: fix randomBytes
    
    * fix ci lint
    
    * ci: add cd flow
    
    * ci: add cd flow

--- src/test/auth.test.ts
+++ src/test/auth.test.ts
@@ -1,18 +1,16 @@
 import { describe, expect, test } from "vitest";
 
 const person = {
-    isActive: true,
-    age: 32,
-}
+  isActive: true,
+  age: 32,
+};
 
 describe("person", () => {
-    test("should be defined", () => {
-        expect(person).toBeDefined();
-    });
+  test("should be defined", () => {
+    expect(person).toBeDefined();
+  });
 
-    test("should be active", () => {
-        expect(person.isActive).toBe(true);
-    });
-
-    
+  test("should be active", () => {
+    expect(person.isActive).toBe(true);
+  });
 });
